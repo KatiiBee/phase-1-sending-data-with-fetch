c49abb7a5d4cd23aaa9d89509aaf910d4f87361b
Katie Demetriff <kdemetriff@gmail.com>
2022-09-27 19:42:47 -0400

    commit tree a4b5e1248411712f38486295f02481327d893bd7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    const configObject ={
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":name,
+            "email":email,
+        })
+    };
+    return fetch ('http://localhost:3000/users', configObject)
+    .then (resp=>resp.json())
+    .then(body=>newContact(body.id))
+
+    .catch (function (error){
+        document.querySelector("body").innerHTML = error
+    })
+}
+function newContact(newID){
+    document.querySelector("body").innerHTML = newID;
+    
+}
+submitData ("Name", "name@name.com")
